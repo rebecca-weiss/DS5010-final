5a51b926615dfa043929836793e5ca3886b0886f
rlemas <rdl161994@aol.com>
2022-04-27 11:59:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+Created on Tue Apr 26 11:27:21 2022
+
+@author: Ruben
+"""
+import pandas as pd
+from round_slicingV2 import Basic_clean
+from sum_statisticV2 import Sum_statistics
+from combined_statisticsV2 import Combined_statistics
+
+
+def main() :
+    # Test Round slicing file, by creating a dataframe from each method
+    bc = Basic_clean("data/co-est2021-alldata.csv", "data/NST-EST2021-alldata.csv")
+    aggregated_regions = bc.aggregated_regions()
+    regions = bc.regions("West")
+    aggregated_states = bc.aggregated_states()
+    states = bc.state_cities("Alabama")
+    city = bc.city("New York", "New York")
+    
+    # Test Sum_Statistics with DataFrame
+    ss = Sum_statistics(aggregated_regions)
+    # the summary method calls all other methods in class, excluding graph
+    print(ss.summary('ESTIMATESBASE2020'))
+    
+    ss = Sum_statistics(regions)
+    print(ss.summary('ESTIMATESBASE2020'))
+    
+    # Test Combines_statistics with DataFrame
+    # Note Combined statistics, is only to compare states and cities
+    cs = Combined_statistics(aggregated_states)
+    # the combined_summary method calls all other methods in class, comparison method
+    print(cs.combined_summary(['Alaska', 'Alabama','Arkansas'], 'NAME', 'BIRTHS2020'))
+    print(cs.comparison('Massachusetts', 'New York', 'NAME', 'ESTIMATESBASE2020'))
+    
+    cs = Combined_statistics(states)
+    print(cs.combined_summary(['Choctaw County', 'Cleburne County','Cullman County'], 'CTYNAME', 'BIRTHS2020'))
+    print(cs.comparison('Choctaw County', 'Cleburne County', 'CTYNAME', 'ESTIMATESBASE2020'))
+
+if __name__ == "__main__":
+    main()
